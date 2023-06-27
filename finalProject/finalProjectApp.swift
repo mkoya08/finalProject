0ff46518a12f1bca7ccee4eeb7787cc64676c593
Moyosore Ogunkoya <momoart188@gmail.com>
2023-06-27 09:48:10 -0400

    Initial Commit

---
+++ finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by Moyo on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
